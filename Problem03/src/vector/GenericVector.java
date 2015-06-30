8d4a00228acb92d931fdb5da9026b7982dcc527f
Sergio García <garciparedes@gmail.com>
2015-06-30 10:32:47 +0200

    Añadidos Javadoc a las clases VectorOps y GenericVector #3

--- Problem03/src/vector/GenericVector.java
+++ Problem03/src/vector/GenericVector.java
@@ -4,10 +4,17 @@
 package vector;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 
 /**
+ * Clase GenericVector.
+ * Esta clase sirve para modelar vectores
+ * en el sistema. Es una clase generica que
+ * permite la entrada de todo tipo de objetos
+ * que implementen la interfaz VectorOps, que
+ * ademas tambien implementa la propia clase para
+ * realizar dichas operaciones.
+ * 
  * @author garciparedes
  *
  */
@@ -16,51 +23,103 @@ public class GenericVector<E extends VectorOps<E>> implements VectorOps<GenericV
 	private ArrayList<E>  vectorList;
 	
 	
+	/**
+	 * Constructor de la clase GenericVector.
+	 */
 	public GenericVector(){
 		vectorList = new ArrayList<E>();
 	}
 	
 	
+	/**
+	 * Constructor de la clase GenericVector.
+	 * 
+	 * @param dimen numero de elementos que tendra el vector.
+	 */
 	public GenericVector(int dimen){
+		assert(dimen > 0);
+
 		vectorList = new ArrayList<E>(dimen);
 	}
 	
 	
+	/**
+	 * Constructor de la clase GenericVector.
+	 * 
+	 * @param elements Lista de elementos con los
+	 * cuales se inicializa el vector.
+	 */
 	@SafeVarargs
 	public GenericVector( E... elements){
 		this();				 
+
+		assert(elements != null);
+		
 		for (E element: elements){
 				addValue(element);
 		}
 	}
 	
 	
-	public void addValue(E complejo){
-		getVector().add(complejo);
+	/**
+	 * Funcion que agrega un elemento al vector.
+	 * 
+	 * @param element elemento a agregar.
+	 */
+	public void addValue(E element){
+		assert(element != null);
+		
+		getVector().add(element);
 	}
 	
 	
+	/**
+	 * Funcion que devuelve la longitud del vector.
+	 * 
+	 * @return longitud del vector.
+	 */
 	public int getDimen(){
 		return getVector().size();
 	}
 	
 	
+	/**
+	 * Funcion que devuelve el elemento
+	 * que esta en el vector en una determinada posicion.
+	 * 
+	 * @param pos posicion en la que esta el elemento.
+	 * @return elemento que se pide.
+	 */
 	public E getValue(int pos){
+		assert(pos >= 0 && pos < getDimen());
+		
 		return getVector().get(pos);
 	}
 	
 	
+	/**
+	 * Funcion que devuelve una
+	 * lista con todos los elementos
+	 * contenidos en el vector de tipo
+	 * ArrayList<E>.
+	 * 
+	 * @return result Lista con elementos del vector.
+	 */
 	public ArrayList<E> getVector(){	
 		return vectorList;
 	}
-	
-	public Iterator<E> getIterator(){
-		return vectorList.iterator();
-	}
 
 
+	/**
+	 * Funcion que suma dos vectores del mismo tipo
+	 * y devuelve un nuevo vector con el resultado.
+	 * 
+	 * @return result Resultado de la operacion.
+	 */
 	@Override
 	public GenericVector<E> suma(GenericVector<E> vector) {
+		assert(vector != null);
+		
 		GenericVector<E> result = new GenericVector<E>(getDimen());	
 		for (int i = 0; i < getDimen(); i++){
 			result.addValue(
@@ -71,6 +130,12 @@ public class GenericVector<E extends VectorOps<E>> implements VectorOps<GenericV
 	}
 
 
+	/**
+	 * Funcion que resta dos vectores del mismo tipo
+	 * y devuelve un nuevo vector con el resultado.
+	 * 
+	 * @return result Resultado de la operacion.
+	 */
 	@Override
 	public GenericVector<E> resta(GenericVector<E> vector) {
 		GenericVector<E> result = new GenericVector<E>(getDimen());	
@@ -83,6 +148,13 @@ public class GenericVector<E extends VectorOps<E>> implements VectorOps<GenericV
 	}
 	
 	
+	/**
+	 * Funcion que devuelve el contenido del vector 
+	 * convertido en una cadena de caracteres de tipo
+	 * String.
+	 * 
+	 * @return result Cadena de caracteres.
+	 */
 	@Override
 	public String toString(){
 		StringBuilder sb = new StringBuilder();		
@@ -96,6 +168,12 @@ public class GenericVector<E extends VectorOps<E>> implements VectorOps<GenericV
 	}
 
 
+	/**
+	 * Funcion que multiplica todo el vector por
+	 * un numero double.
+	 * 
+	 * @return result Resultado de la operacion.
+	 */
 	@Override
 	public GenericVector<E> multiplicacion(double value) {
 		GenericVector<E> result = new GenericVector<E>(getDimen());
@@ -106,6 +184,12 @@ public class GenericVector<E extends VectorOps<E>> implements VectorOps<GenericV
 	}
 
 
+	/**
+	 * Funcion que divide todo el vector entre
+	 * un numero double.
+	 * 
+	 * @return result Resultado de la operacion.
+	 */
 	@Override
 	public GenericVector<E> division(double value) {
 		GenericVector<E> result = new GenericVector<E>(getDimen());	
--- Problem03/src/vector/VectorOps.java
+++ Problem03/src/vector/VectorOps.java
@@ -1,17 +1,52 @@
 package vector;
-
+/**
+ * Interfaz destinada a la genericidad 
+ * de la la clase GenericVector
+ * 
+ * @author garciparedes
+ *
+ * @param <T>
+ */
 public interface VectorOps<T> {
 
 	
+	/**
+	 * Funcion que suma dos objetos del mismo
+	 * tipo y devuelve el resultado en otro objeto nuevo.
+	 * 
+	 * @param value Segundo Sumando.
+	 * @return result Resultado de la suma.
+	 */
 	public T suma(T value);
 
 	
+	/**
+	 * Funcion que resta dos objetos del mismo
+	 * tipo y devuelve el resultado en otro objeto nuevo.
+	 * 
+	 * @param value Valor a restar.
+	 * @return result Resultado de la resta.
+	 */
 	public T resta(T value);
 	
 	
+	/**
+	 * Funcion que multiplica el objeto por
+	 * un numero double.
+	 * 
+	 * @param value double por el que multiplicar.
+	 * @return result Resultado de la operacion.
+	 */
 	public T multiplicacion(double value);
 	
 	
+	/**
+	 * Funcion que divide el objeto por
+	 * un numero double.
+	 * 
+	 * @param value double entre el que dividir.
+	 * @return result Resultado de la operacion.
+	 */
 	public T division(double value);
 
 }
